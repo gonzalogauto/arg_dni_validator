90fb38ac81a28a3c82ff59d3d9bd5647e83e64b7
Gonzaa25 <gonzalo.gauto@gmail.com>
2021-04-13 18:05:42 -0300

    adding dart comment

--- lib/arg_dni_validator.dart
+++ lib/arg_dni_validator.dart
@@ -1,6 +1,10 @@
 library arg_dni_validator;
 
 class DniValidator {
+  /// Validates DNI.
+  ///
+  /// Returns false if DNI is invalid.
+  /// Returns true if DNI is valid
   bool isValidDNI(String dni) {
     final String dnivalidator = r"^\d{8}(?:[-\s]\d{4})?$";
     if (RegExp(dnivalidator, caseSensitive: true, multiLine: true)
