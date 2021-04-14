c98276ab498f43f63750cd8a78856f9fac2c1322
Gonzaa25 <gonzalo.gauto@gmail.com>
2021-04-13 21:09:11 -0300

    rename class --adding cuit validator

--- lib/arg_dni_validator.dart
+++ lib/arg_dni_validator.dart
@@ -1,16 +1,30 @@
 library arg_dni_validator;
 
-class DniValidator {
+class ArgValidator {
+  final String _dnivalidator = r"^\d{8}(?:[-\s]\d{4})?$";
+  final String _cuitExp = r'\b(20|23|24|27|30|33|34)(-)?[0-9]{8}(-)?[0-9]';
+
   /// Validates DNI.
   ///
   /// Returns false if DNI is invalid.
-  /// Returns true if DNI is valid
+  /// Returns true if DNI is valid.
   bool isValidDNI(String dni) {
-    final String dnivalidator = r"^\d{8}(?:[-\s]\d{4})?$";
-    if (RegExp(dnivalidator, caseSensitive: true, multiLine: true)
+    if (RegExp(_dnivalidator, caseSensitive: true, multiLine: true)
         .hasMatch(dni)) {
       return true;
     }
     return false;
+  }
+
+  /// Validates CUIT.
+  ///
+  /// Returns false if CUIT is invalid.
+  /// Returns true if CUIT is valid.
+  bool isValidCUIT(String text) {
+    if (RegExp(_cuitExp).hasMatch(text)) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
